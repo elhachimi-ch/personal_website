9b2b9023f0014a2ddadbcd63af31a9df576f7ef8
elhachimi-ch <elhachimi.ch@gmail.com>
2026-09-07 17:58:00 +0800

    Add new audio file 'brawl_iraqi_wtar.m4a' and update audio library in mode-effects.js

--- assets/mode-effects.js
+++ assets/mode-effects.js
@@ -98,7 +98,8 @@
             '3ayta_wald_9addour_brawl.mp3',
             'swakn_talab_ya_lf9ih.m4a',
             'moulay_abdellah_bnt_houcine.mp3',
-            'kassi_frid_zerhouni.mp3'
+            'kassi_frid_zerhouni.mp3',
+            'brawl_iraqi_wtar.m4a'
         ]
     };
     const VALID_GENRES = new Set([...Object.keys(AUDIO_LIBRARY), 'no']);
